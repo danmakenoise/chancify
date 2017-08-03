1c4ab845b54f7442fb38171059a4ca6b306031e1
Dan Phillips <danmakenoise@gmail.com>
2017-08-03 13:43:57 -0400

    chancify works with 100% chance

--- src/chancify.test.js
+++ src/chancify.test.js
@@ -2,12 +2,30 @@
 import test from 'tape';
 import chancify from './chancify';
 
-test('chancify()', (t) => {
+const runChancifyTest = (t: any): void => {
+  const numTestsToRun = 10000;
+  let timesRun = 0;
+
+  const testFunction = () => { timesRun += 1; };
+
+  for (let i = 0; i < numTestsToRun; i += 1) {
+    chancify(testFunction, 1 / 1);
+  }
+
+  t.is(
+    timesRun === numTestsToRun,
+    'it runs 100% of the time',
+  );
+};
+
+test('chancify()', (t): void => {
   t.is(
     typeof chancify,
     'function',
     'it is a function',
   );
+
+  runChancifyTest(t);
 
   t.end();
 });
