7b5413364df9d6d1835560fb442b6324d9a0ec4f
Dan Phillips <danmakenoise@gmail.com>
2017-08-03 14:13:19 -0400

    chancify passes all tests

--- src/chancify.js
+++ src/chancify.js
@@ -1,4 +1,18 @@
 // @flow
-const chancify = () => {};
+type ChancifyOptsType = {
+  outOf: number,
+  runFor: number,
+};
+
+function chancify(func: Function, opts: ChancifyOptsType): void {
+  const { outOf, runFor } = opts;
+
+  const percentChance = runFor / outOf;
+  const shouldRunFunc = Math.random() <= percentChance;
+
+  if (shouldRunFunc) {
+    func();
+  }
+}
 
 export default chancify;
